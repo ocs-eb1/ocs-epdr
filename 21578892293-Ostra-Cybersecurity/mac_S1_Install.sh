6fd7c8f085703d53b325ab296e5437852569dbfe
chetan-ostra <chetan.maddela@ostra.net>
2025-03-04 11:21:05 +0530

    ostra

--- 21578892293-Ostra-Cybersecurity/mac_S1_Install.sh
+++ 21578892293-Ostra-Cybersecurity/mac_S1_Install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-siteToken="eyJ1cmwiOiAiaHR0cHM6Ly91c2VhMS0wMDItbXNzcC5zZW50aW5lbG9uZS5uZXQiLCAic2l0ZV9rZXkiOiAiNGJjZGQ1ZDJmMjk4NDhjNCJ9"
+siteToken="https://github.com/ocs-eb1/ocs-epdr/blob/main/21578892293-Ostra-Cybersecurity/ostra-token.txt"
 installerurl="https://raw.githubusercontent.com/ocs-eb1/ocs-epdr/refs/heads/main/Sentinel-Release-23-3-2-7123_macos_v23_3_2_7123.pkg"
 installer="Sentinel-Release-23-3-2-7123_macos_v23_3_2_7123.pkg"
 dir="/tmp/"
@@ -8,17 +8,9 @@ tokenfile="com.sentinelone.registration-token"
 if [-d /Application/SentinelOne/ ];
 then
         echo "SentinelOne is already Installed"
-
-
 else
-        wget $dir$installer $dirinstallerurl
-
-
+        wget $dir$installer $dirinstallerurl $siteToken
         echo $siteToken > $dir$tokenfile
-
-
         /usr/sbin/installer -pkg $dir$installer -target /
-
         rm -f $dir$tokenfile
-
 fi
