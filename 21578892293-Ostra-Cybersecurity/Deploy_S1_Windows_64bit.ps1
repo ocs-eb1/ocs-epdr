c5451d76be791222868edb3597e25cae3252acdb
chetan-ostra <chetan.maddela@ostra.net>
2025-03-04 11:15:47 +0530

    ostra

--- 21578892293-Ostra-Cybersecurity/Deploy_S1_Windows_64bit.ps1
+++ 21578892293-Ostra-Cybersecurity/Deploy_S1_Windows_64bit.ps1
@@ -2,13 +2,14 @@
 if (!(Test-Path  $Local_Dest)) {
     New-Item -Path $Local_Dest -ItemType Directory -Force | Out-Null
 }
-https://github.com/ocs-eb1/ocs-epdr/blob/ostra/
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/ocs-eb1/ocs-epdr/refs/heads/main/Ostra-site-token.mst -OutFile $Local_Dest\Ostra-site-token.mst
-$token = Get-ChildItem "C:\Temp\Ostra-site-token.mst"
+#https://github.com/ocs-eb1/ocs-epdr/blob/ostra/
+Invoke-WebRequest -Uri https://github.com/ocs-eb1/ocs-epdr/blob/main/21578892293-Ostra-Cybersecurity/ostra-token.txt -OutFile $Local_Dest\Ostra-site-token.txt
+#Invoke-WebRequest -Uri https://raw.githubusercontent.com/ocs-eb1/ocs-epdr/refs/heads/main/Ostra-site-token.mst -OutFile $Local_Dest\Ostra-site-token.mst
+$token = Get-ChildItem "C:\Temp\Ostra-site-token.txt"
 $url = "https://raw.githubusercontent.com/ocs-eb1/ocs-epdr/refs/heads/main/SentinelInstaller_windows_64bit_v23_3_4_320.msi"
 $file= Split-Path $url -Leaf 
 $out_path = "C:\Temp\SentinelOneInstaller_windows_64bit_v23_3_4_320.msi"
 $agent = (Get-ChildItem $Local_Dest -Filter "Sentinel*.msi").FullName
 Invoke-WebRequest $url -OutFile $out_path
-Start-Process msiexec.exe -ArgumentList " /i $agent TRANSFORMS=$($token) /qn" -Wait
+Start-Process msiexec.exe -ArgumentList " /i $agent $token /qn" -Wait
 
--- 21578892293-Ostra-Cybersecurity/Linux_s1.sh
+++ 21578892293-Ostra-Cybersecurity/Linux_s1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sentinelToken="eyJ1cmwiOiAiaHR0cHM6Ly91c2VhMS0wMDItbXNzcC5zZW50aW5lbG9uZS5uZXQiLCAic2l0ZV9rZXkiOiAiNGJjZGQ1ZDJmMjk4NDhjNCJ9"
+sentinelToken="https://github.com/ocs-eb1/ocs-epdr/blob/main/21578892293-Ostra-Cybersecurity/ostra-token.txt"
 downloadLink="https://raw.githubusercontent.com/ocs-eb1/ocs-epdr/refs/heads/main/SentinelAgent_linux_x86_64_v23_3_2_12.deb"
 pkgName="SentinelAgent_linux_x86_64_v23_3_2_12.deb"
 
@@ -15,7 +15,7 @@ else
 cd /tmp
 
 #Download Agent
-wget $pkgName $downloadLink
+wget $pkgName $downloadLink $sentinelToken
 
 #Install Agent
 #chmod +x $pkgName
